73b32ed2e8840f7ab28c48097e65f2edee99f40e
TeoryMan <jonathancortestm143@gmail.com>
2023-05-16 21:17:12 -0500

    circumference page

--- src/pages/Circumference/Circumference.tsx
+++ src/pages/Circumference/Circumference.tsx
@@ -1,4 +1,15 @@
+import Article from '../../components/Article';
+import Image from '../../components/Image';
+import Title from '../../components/Title';
+
 function Circumference() {
-    return <div>Circumference</div>;
+    return (
+        <>
+            <Title>Circumferencia</Title>
+            <Article className='flex justify-center'>
+                <Image src='src/pages/Circumference/assets/circum.jpg' />
+            </Article>
+        </>
+    );
 }
 export default Circumference;
